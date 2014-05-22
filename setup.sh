7d6654d93b2a24a4738fecf797dd4ce455238020
Kawashima Daichi <daichi0.0daichi@gmail.com>
2014-05-22 23:44:23 +0900

    hoge

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@
 
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
 
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
