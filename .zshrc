5da8f4141aca02250f524894ef0ee28828c4763d
Daichi Kasahima <daichi0.0daichi@gmail.com>
2014-05-22 23:41:48 +0900

    modified

--- .zshrc
+++ .zshrc
@@ -10,6 +10,12 @@ elif [ `uname` = 'Linux' ]; then
 	export LSCOLORS=exfxcxdxbxegedabagacad
 	export LS_COLORS='di=36:ln=35:so=32:pi=33:ex=31:bd=46;36:cd=43;36:su=41;30:sg=46;30:tw=42;30:ow=43;30'
 	zstyle ':completion:*' list-colors 'di=36' 'ln=35' 'so=32' 'ex=31' 'bd=46;36' 'cd=43;36'
+elif [ `uname` = 'FreeBSD' ]; then
+	echo 'FreeBSD'
+	autoload -U compinit
+	alias ls="ls -G -F"
+	compinit
+	export LSCOLORS=gxfxcxdxbxegedabagacad
 fi
 
 
--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-#DOT_FILES=( .zshrc .vimrc .vim .bash_profile)
+#DOT_FILES=( .zshrc .vimrc .bash_profile)
 
 ln -s $HOME/dotfiles/.zshrc $HOME/.zshrc
 ln -s $HOME/dotfiles/.vimrc $HOME/.vimrc
-ln -s $HOME/dotfiles/.vim $HOME/.vim
 ln -s $HOME/dotfiles/.bash_profile $HOME/.bash_profile
 
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle && git clone git://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim && echo "you should run following command to setup plugins ->  vim -c ':NeoBundleInstall'"
